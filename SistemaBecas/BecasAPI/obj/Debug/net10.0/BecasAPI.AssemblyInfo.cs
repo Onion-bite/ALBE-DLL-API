b1500189602fc4ef9810fd8a137c25be2ed0f57d
AxelC2 <caxel0624@gmail.com>
2026-05-25 17:42:14 -0500

    fix: delete fill deprecated and add becas.json

--- SistemaBecas/BecasAPI/obj/Debug/net10.0/BecasAPI.AssemblyInfo.cs
+++ SistemaBecas/BecasAPI/obj/Debug/net10.0/BecasAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BecasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49e0bd5e76fac9e68bea667eb6e2729a9212a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78872386cbe6aff6bb60179a553d1a452edd765")]
 [assembly: System.Reflection.AssemblyProductAttribute("BecasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BecasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
