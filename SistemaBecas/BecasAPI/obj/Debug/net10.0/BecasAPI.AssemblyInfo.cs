0cb80652ad04d3435211c0777886e998758f398d
AxelC2 <caxel0624@gmail.com>
2026-05-25 19:25:37 -0500

    chore: actualizacion de datos con postman
    Co-Authored-By: LisetteAra <286133231+LisetteAra@users.noreply.github.com>
    Co-Authored-By: nilto123xd <219251578+nilto123xd@users.noreply.github.com>

--- SistemaBecas/BecasAPI/obj/Debug/net10.0/BecasAPI.AssemblyInfo.cs
+++ SistemaBecas/BecasAPI/obj/Debug/net10.0/BecasAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BecasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1500189602fc4ef9810fd8a137c25be2ed0f57d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0858c035208811afbd4e4b55113802d553c8569")]
 [assembly: System.Reflection.AssemblyProductAttribute("BecasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BecasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
